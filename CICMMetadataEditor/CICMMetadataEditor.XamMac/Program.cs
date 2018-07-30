ac2efe2bac9e8ec3b5763ac5d97a4d6f4175ef4b
Natalia Portillo <claunia@claunia.com>
2018-07-30 21:04:27 +0100

    Add license.

--- CICMMetadataEditor/CICMMetadataEditor.XamMac/Program.cs
+++ CICMMetadataEditor/CICMMetadataEditor.XamMac/Program.cs
@@ -1,4 +1,35 @@
-﻿using AppKit;
+﻿// /***************************************************************************
+// Canary Islands Computer Museum Metadata Editor
+// ----------------------------------------------------------------------------
+//
+// Filename       : MainForm.xeto.cs
+// Author(s)      : Natalia Portillo <claunia@claunia.com>
+//
+// Component      : Eto.XamMac
+//
+// --[ Description ] ----------------------------------------------------------
+//
+//     Contains startup code for Eto framework under Xamarin.Mac
+//
+// --[ License ] --------------------------------------------------------------
+//
+//     This library is free software; you can redistribute it and/or modify
+//     it under the terms of the GNU Lesser General Public License as
+//     published by the Free Software Foundation; either version 2.1 of the
+//     License, or (at your option) any later version.
+//
+//     This library is distributed in the hope that it will be useful, but
+//     WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+//     Lesser General Public License for more details.
+//
+//     You should have received a copy of the GNU Lesser General Public
+//     License along with this library; if not, see <http://www.gnu.org/licenses/>.
+//
+// ----------------------------------------------------------------------------
+// Copyright © 2018 Natalia Portillo
+// ****************************************************************************/
+using AppKit;
 using Eto.Forms;
 
 namespace CICMMetadataEditor.XamMac
