6fabcdf694e7ef0758eb9f6cb4ed7340632588ba
Natalia Portillo <claunia@claunia.com>
2020-07-12 22:04:18 +0100

    Add missing Java files.

---
+++ java/generated/TrackIndexType.java
@@ -0,0 +1,100 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2020.07.12 a las 10:02:34 PM WEST 
+//
+
+
+package generated;
+
+import java.math.BigInteger;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.bind.annotation.XmlValue;
+
+
+/**
+ * <p>Clase Java para TrackIndexType complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="TrackIndexType">
+ *   &lt;simpleContent>
+ *     &lt;extension base="&lt;http://www.w3.org/2001/XMLSchema>integer">
+ *       &lt;attribute name="index" use="required">
+ *         &lt;simpleType>
+ *           &lt;restriction base="{http://www.w3.org/2001/XMLSchema}integer">
+ *           &lt;/restriction>
+ *         &lt;/simpleType>
+ *       &lt;/attribute>
+ *     &lt;/extension>
+ *   &lt;/simpleContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "TrackIndexType", propOrder = {
+    "value"
+})
+public class TrackIndexType {
+
+    @XmlValue
+    protected BigInteger value;
+    @XmlAttribute(name = "index", required = true)
+    protected BigInteger index;
+
+    /**
+     * Obtiene el valor de la propiedad value.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getValue() {
+        return value;
+    }
+
+    /**
+     * Define el valor de la propiedad value.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setValue(BigInteger value) {
+        this.value = value;
+    }
+
+    /**
+     * Obtiene el valor de la propiedad index.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigInteger }
+     *     
+     */
+    public BigInteger getIndex() {
+        return index;
+    }
+
+    /**
+     * Define el valor de la propiedad index.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigInteger }
+     *     
+     */
+    public void setIndex(BigInteger value) {
+        this.index = value;
+    }
+
+}
